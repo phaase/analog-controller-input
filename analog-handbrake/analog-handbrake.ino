e4e013fa76bfdde20e51468fe0a09f978c758fc4
phaase <kontakt@phaase.net>
2017-10-06 18:24:37 +0200

    Use YAxis instead of brake value which is not supported by Project Cars

--- analog-handbrake/analog-handbrake.ino
+++ analog-handbrake/analog-handbrake.ino
@@ -48,7 +48,7 @@ const int maxValue = 900; // max of the actual usable poti range
 /*  Use this to set the time in milli seconds after which a send operation is forced even if the value has not changed
  *  or, to be more precisely, if the change is below changeTolerance (see below).
  */
-const unsigned long maxQuietTime = 5000; //time after that send operation is called (indirectly by calling setBrake) even when value has not changed
+const unsigned long maxQuietTime = 5000; //time after that send operation is called (indirectly by calling setYAxis) even when value has not changed
 
 /* Use this to set the sensitiy of the controller. Setting the value to 0 means no tolerance = max sensity = max send operations, 
  * setting to 1 means high sensity but some tolerance against jitter between two values and hence might reduce the frequency of 
@@ -59,12 +59,12 @@ const int changeTolerance = 1; //set and sent new value when change of value > c
 const int potPin = A0;    // select the input pin for the potentiometer
 const int ledPin = LED_BUILTIN;   // select the pin for the LED  
 
-// Create Joystick, disable anything than the brake which you can map in a game as whatever you want
+// Create Joystick, disable anything than the Y-axis (using 'brake' does not work with project cars) which you can map in a game as whatever you want
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
     0, // JOYSTICK_DEFAULT_BUTTON_COUNT
     0, // JOYSTICK_DEFAULT_HATSWITCH_COUNT,
     false, // includeXAxis
-    false, // includeYAxis
+    true,  // includeYAxis
     false, // includeZAxis
     false, // includeRxAxis
     false, // includeRyAxis
@@ -72,19 +72,19 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
     false, // includeRudder
     false, // includeThrottle
     false, // includeAccelerator
-    true, // includeBrake
+    false, // includeBrake
     false); // includeSteering
   
 int ledState = HIGH;
 int oldValue = 0;
 
 unsigned long previousMillisBlink = 0; //stores last time LED was updated
-unsigned long previousMillisSend = 0; //stores last time send operation was called (indirectly by calling setBrake)
+unsigned long previousMillisSend = 0; //stores last time send operation was called (indirectly by calling setYAxis)
 
 void setup() {
   Serial.begin(9600);      // open the serial port at 9600 bps
   pinMode(ledPin, OUTPUT);  // declare the ledPin as an OUTPUT
-  Joystick.setBrakeRange(minValue, maxValue);
+  Joystick.setYAxisRange(minValue, maxValue);
   Joystick.begin();
   digitalWrite(ledPin, ledState);  // turn the ledPin on
   oldValue = -abs(changeTolerance)-1; //force send the very first time
@@ -95,7 +95,7 @@ void loop() {
   unsigned long currentMillis = millis();
   int value = analogRead(potPin);    // read the value from the sensor
   if((currentMillis-previousMillisSend > maxQuietTime) || (abs(value-oldValue) > changeTolerance)){
-    Joystick.setBrake(value);
+    Joystick.setYAxis(value);
     oldValue = value;
     previousMillisSend = currentMillis;
     #ifdef CALIBRATION_MODE
